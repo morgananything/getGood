a6bc593d018c20036cca9712c9867207eabe1e6f
morgananything <morganjermain@hotmail.com>
2020-02-26 21:35:57 +0000

    aaa

--- createhours.js
+++ createhours.js
@@ -3,6 +3,57 @@ function createHours(grade){ //grade is inputted. we're judging based on this.
     var scoreN = parseInt(score);
     var grade = prompt("What grade did you achieve in this assessment?")
     var subject = document.getElementById('subject').options[document.getElementById('subject').selectedIndex].text;
+    
+    if(subject == "Other"){
+        subject = prompt("What is the subject being evaluated?")
+    } else{
+        subject = subject;
+    }
+    var scoreO;
+        switch(grade){
+            case "A":
+                scoreO = scoreN; 
+                break;
+            case "B":
+            case "C":
+                scoreO = scoreN + 1; 
+                break;
+            case "D":
+            case "E":
+            case "F":
+                scoreO = scoreN + 3;
+                break;
+            case "G":
+            case "U":
+                scoreO = scoreN + 5;
+                break;
+        }
+
+    if(score < 0){
+        document.getElementById("failedA").innerHTML = 
+        "You fool. You bastard. Stop making a mockery of my code. You can't revise for negative hours.";
+    } else if(scoreO > score){
+        document.getElementById("tableA").innerHTML = "You should revise for "+ scoreO + " hours.";
+    } else if(scoreO == score){
+        document.getElementById("tableA").innerHTML = "You're doing great in " + subject + "!";
+    } else{
+        document.getElementById("tableA").innerHTML = 
+        "Stop messing around right now. This is serious. <br> Input valid values.";
+    }
+
+    document.getElementById("hoursA").innerHTML = score;
+    document.getElementById("gradeA").innerHTML = grade;
+    document.getElementById("newA").innerHTML = scoreO;
+    document.getElementById("subjectA").innerHTML = subject;
+    document.getElementById("add1").style.display = "inline-block";
+    
+}
+
+function more1(grade){ 
+    var subject = document.getElementById('subject1').options[document.getElementById('subject1').selectedIndex].text;
+    var score = prompt("How many hours this week did you revise?")
+    var scoreN = parseInt(score);
+    var grade = prompt("What grade did you achieve in this assessment?")
     if(subject == "Other"){
         subject = prompt("What is the subject being evaluated?")
     } else{
@@ -37,65 +88,61 @@ function createHours(grade){ //grade is inputted. we're judging based on this.
         document.getElementById("tableA").innerHTML = "You're doing great!";
     }else{
         document.getElementById("tableA").innerHTML = 
-        "Stop messing around right now. This is serious. <br> Prepare your values with the <br> dropdowns and input valid values.";
+        "Stop messing around right now. This is serious. Prepare your values with the dropdowns and input valid values.";
     }
-
-    document.getElementById("hoursA").innerHTML = score;
-    document.getElementById("gradeA").innerHTML = grade;
-    document.getElementById("newA").innerHTML = scoreO;
-    document.getElementById("subjectA").innerHTML = subject;
-    
-    var more = document.getElementById("more");
-  if (more.style.display === "block") {
-    more.style.display = "none";
-  } else {
-    
+    document.getElementById("hoursB").innerHTML = score;
+    document.getElementById("gradeB").innerHTML = grade;
+    document.getElementById("newB").innerHTML = scoreO;
+    document.getElementById("subjectB").innerHTML = subject;
+    document.getElementById("hidden1").style.display = "none";
+    document.getElementById("add1").style.display = "none";
 }
-/////////
-function more(grade1){ 
-    var subject1 = document.getElementById('subject1').options[document.getElementById('subject1').selectedIndex].text;
-    var score1 = prompt("How many hours this week did you revise?")
-    var scoreN1 = parseInt(score1);
-    var grade1 = prompt("What grade did you achieve in this assessment?")
+
+function more2(grade){ 
+    var subject = document.getElementById('subject2').options[document.getElementById('subject2').selectedIndex].text;
+    var score = prompt("How many hours this week did you revise?")
+    var scoreN = parseInt(score);
+    var grade = prompt("What grade did you achieve in this assessment?")
     if(subject == "Other"){
         subject = prompt("What is the subject being evaluated?")
     } else{
         subject = subject;
     }
-    var scoreO1;
-        switch(grade1){
+    var scoreO;
+        switch(grade){
             case "A":
-                scoreO1 = scoreN1; 
+                scoreO = scoreN; 
                 break;
             case "B":
             case "C":
-                scoreO1 = scoreN1 + 1; 
+                scoreO = scoreN + 1; 
                 break;
             case "D":
             case "E":
             case "F":
-                scoreO1 = scoreN1 + 3;
+                scoreO = scoreN + 3;
                 break;
             case "G":
             case "U":
-                scoreO1 = scoreN1 + 5;
+                scoreO = scoreN + 5;
                 break;
         }
 
-    if(score1 < 0){
+    if(score < 0){
         document.getElementById("failedA").innerHTML = 
         "You fool. You bastard. Stop making a mockery of my code. You can't revise for negative hours.";
-    } else if(scoreO1 > score1){
-        document.getElementById("tableA").innerHTML = "You should revise for "+ scoreO1 + " hours.";
-    } else if(scoreO1 == score1){
+    } else if(scoreO > score){
+        document.getElementById("tableA").innerHTML = "You should revise for " + subject "for " + scoreO + " hours.";
+    } else if(scoreO == score){
         document.getElementById("tableA").innerHTML = "You're doing great!";
     }else{
         document.getElementById("tableA").innerHTML = 
         "Stop messing around right now. This is serious. Prepare your values with the dropdowns and input valid values.";
     }
-    document.getElementById("hoursB").innerHTML = score1;
-    document.getElementById("gradeB").innerHTML = grade1;
-    document.getElementById("newB").innerHTML = scoreO1;
-    document.getElementById("subjectB").innerHTML = subject1;
-    document.getElementById("more").innerHTML = "add more?";
+    document.getElementById("hoursC").innerHTML = score;
+    document.getElementById("gradeC").innerHTML = grade;
+    document.getElementById("newC").innerHTML = scoreO;
+    document.getElementById("subjectC").innerHTML = subject;
+    document.getElementById("hidden2").style.display = "none";
+    document.getElementById("add2").style.display = "none";
 }
